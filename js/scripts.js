9178bafaf7cc3fc61455c0f1f1754940f76b3be4
Isih97 <isabelhansen97@yahoo.de>
2021-11-23 18:00:05 +0100

    Logo converted

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,10 @@ window.addEventListener('DOMContentLoaded', event => {
         }
         if (window.scrollY === 0) {
             navbarCollapsible.classList.remove('navbar-shrink')
+            document.getElementById("logo").src = "../assets/img/icons/logo-white.png"
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
+            document.getElementById("logo").src = "../assets/img/icons/logo-black.png"
         }
 
     };
