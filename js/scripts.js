b8302069520e4491a5a74e6f0e8e82925b530447
Isih97 <isabelhansen97@yahoo.de>
2022-01-03 12:30:18 +0100

    Version Karte + Wordcloud Buchstaben

--- js/scripts.js
+++ js/scripts.js
@@ -46,7 +46,8 @@ window.addEventListener('DOMContentLoaded', event => {
             offset: 74,
         });
     };
-    
+
+  
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
