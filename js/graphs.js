f42d21dcceba726872870a2f697566a464de3121
Nora <s2nodirl@uni-trier.de>
2022-01-27 12:48:12 +0100

    vieztrends, etymologie-text

--- js/graphs.js
+++ js/graphs.js
@@ -4,7 +4,7 @@ var graphArray = ["figV.html","figA.html", "figC.html","figAe.html"];
 function displayGraph(){
       var click = document.getElementById("selectMenu");
       var newSrcInd = click.options[click.selectedIndex].value;
-      var newSrc = "assets/img/maps/"+graphArray[newSrcInd];
+      var newSrc = "vieztrends/"+graphArray[newSrcInd];
 
       document.getElementById("map").src=newSrc;
      }
