880f25a585f396dc260647dabe4e5bef37024402
Isih97 <isabelhansen97@yahoo.de>
2022-02-10 17:59:31 +0100

    finale Änderungen

--- js/scripts.js
+++ js/scripts.js
@@ -99,69 +99,83 @@ var canvas = document.getElementById('bar-chart');
 new Chart(canvas, {
   type: 'line',
   data: {
-    labels: ['Römerviez', 'FrancoViez', 'WiltingerViez', 'WeyandsViez', 'Weingut Gehlen'],
+    labels: ['Marc Conrad Welschbilliger (Welschbillig)', 'Weyand\'s Viez (Biewer)', 'Joachim Meyer Holzäpelchi (Waldrach)', 'Klimmes (Ralingen)', 'Wolfgang Schmitt Sorte Barbe (Merzig)', 'Römerviez (Welschbillig)', 'Merziger Alter Särkower (Merzig)', 'Franco Viez Juppis Viez (Welschbillig)', 'Rudi Müller (Pluwig)', 'Wiltinger Glühviez (Wiltingen)'],
     datasets: [{
       label: 'A',
       yAxesID: 'A',
-      data: [6, 2, 9, 4, 5],
+      data: [4, 3, 7, 4, 9,2,6,5,4,6],
       borderColor: "#307a44",
       backgroundColor: "rgba(48, 122, 68,0.2)"
     }, {
       label: 'B',
       yAxesID: 'B',
-      data: [3, 4, 5, 6, 2],
+      data: [3, 4, 9, 7, 10,1,7,6,9,10],
       borderColor: "#f46239",
       backgroundColor: "rgba(244, 98, 57,0.2)"
     },
     {
         label: 'C',
         yAxesID: 'C',
-        data: [7, 8, 9, 10, 10],
+        data: [5,4, 7, 9, 2, 0,8,6,8,10],
         borderColor: "#503ade",
         backgroundColor: "rgba(80, 58, 222,0.2)"
       },
       {
         label: 'D',
         yAxesID: 'D',
-        data: [1, 2, 6, 2, 8],
+        data: [2, 1, 9, 4, 10,0,1,5,7,8],
         borderColor: "#f04232",
         backgroundColor: "rgba(201, 122, 56,0.2)"
       },
       {
         label: 'E',
         yAxesID: 'E',
-        data: [4, 1, 0, 9, 5],
+        data: [2, 5, 7, 8, 10,4,9,5,6,10],
         borderColor: "#10ff08",
         backgroundColor: "rgba(16, 255, 8,0.2)"
       },
       {
         label: 'F',
         yAxesID: 'F',
-        data: [1, 1, 4, 5, 3],
+        data: [5, 4, 3, 6, 9,0,5,2,2,10],
         borderColor: "#f5d836",
         backgroundColor: "rgba(245, 216, 54,0.2)"
       },
       {
         label: 'G',
         yAxesID: 'G',
-        data: [7, 2, 1, 6, 7],
+        data: [3, 6, 7, 8, 10,2,9,5,6,10],
         borderColor: "#5cd6f7",
         backgroundColor: "rgba(92, 214, 247,0.2)"
       },
       {
         label: 'H',
         yAxesID: 'H',
-        data: [3, 4, 5, 6, 2],
+        data: [3, 5, 6, 4, 8,1,5,4,6,9],
         borderColor: "#ff08d6",
         backgroundColor: "rgba(255, 8, 214,0.2)"
       },
       {
         label: 'I',
         yAxesID: 'I',
-        data: [1, 8,4, 4, 9],
+        data: [4, 5,8, 5,10,2,4,6,7,2],
         borderColor: "#321e54",
         backgroundColor: "rgba(50, 30, 84,0.2)"
-      }]
+      },{
+        label: 'J',
+        yAxesID: 'J',
+        data: [5, 4,7, 6,8,3,5,4,3,9],
+        borderColor: "#b0bf0a",
+        backgroundColor: "rgba(176, 191, 10,0.2)"
+      },
+      {
+        label: 'K',
+        yAxesID: 'K',
+        data: [0,2, 6,6, 9,3,3,5,1,7],
+        borderColor: "#117d5b",
+        backgroundColor: "rgba(17, 125, 91,0.2)"
+      },
+      ]
   },
   options: {
     scales: {
@@ -179,6 +193,37 @@ new Chart(canvas, {
       {
         id: 'C',
         position: {display:false},
+      },
+      {
+        id: 'D',
+        position: {display:false},
+      },
+      {
+        id: 'E',
+        position: {display:false},
+      },
+      {
+        id: 'F',
+        position: {display:false},
+      },
+      {
+        id: 'G',
+        position: {display:false},
+      },
+      {
+        id: 'H',
+        position: {display:false},
+      },
+      {
+        id: 'I',
+        position: {display:false},
+      },{
+        id: 'J',
+        position: {display:false},
+      },
+      {
+        id: 'K',
+        position: {display:false},
       }]
     }
   }
@@ -188,12 +233,12 @@ new Chart(canvas, {
 new Chart(document.getElementById("bar-chart-2"), {
     type: 'bar',
     data: {
-      labels: ['RömerViez', 'FrancoViez', 'WiltingerViez', 'WeyandsViez', 'Weingut Gehlen'],
+      labels: ['Marc Conrad Welschbilliger (Welschbillig)', 'Weyand\'s Viez (Biewer)', 'Joachim Meyer Holzäpelchi (Waldrach)', 'Klimmes (Ralingen)', 'Wolfgang Schmitt Sorte Barbe (Merzig)', 'Römerviez (Welschbillig)', 'Merziger Alter Särkower (Merzig)', 'Franco Viez Juppis Viez (Welschbillig)', 'Rudi Müller (Pluwig)', 'Wiltinger Glühviez (Wiltingen)'],
       datasets: [
         {
-          label: "Süß (10) - Sauer (0)",
-          backgroundColor: ["#fbb373", "#f46239","#a73616","#e8c3b9","#c45850"],
-          data: [1,5,7,2,4]
+          label: "Mild (10) - Sauer (0)",
+          backgroundColor: ["#fbb373", "#f46239","#a73616","#e8c3b9","#c45850","#fbb373", "#f46239","#a73616","#e8c3b9","#c45850"],
+          data: [31,39,70,62,88,15,57,49,56,83]
         }
       ]
     },
@@ -202,7 +247,7 @@ new Chart(document.getElementById("bar-chart-2"), {
       scales: {
           yAxes: [{
             ticks:{
-                max:10,
+                max:90,
                 min:0
             }
           }]
